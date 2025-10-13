5f16e062d5424a925f2fbc39a00fd5f3cc4d9ca0
corentin.chan <chancorentin@hotmail.com>
2025-10-13 15:20:43 +0200

    rajout search function

--- RecipeProject/frontend/search.js
+++ RecipeProject/frontend/search.js
@@ -1,8 +1,28 @@
 
 recipesJSON = {};
 
+
+
+function recipeSearch_start(){
+
+let tag=document.getElementById("textSearch").value;
+
+if (tag==="") alert("no recipe found");
+else {
+
+recipeDisplay(  searchRecipe(tag));
+}
+
+}
+
+function recipeDisplay(){
+
+        
+
+  }
+
 // search recipes from TheMealDB API
-searchRecipe("chicken")
+//searchRecipe("chicken")
  function searchRecipe(tag){
     let url = "https://www.themealdb.com/api/json/v1/1/filter.php?i="+tag;
           fetch(url)
@@ -19,15 +39,54 @@ searchRecipe("chicken")
           console.log("--------------");
         });
       } else {
-        console.log("Aucune recette trouvée pour :", tag);
+        alert("Aucune recette trouvée pour :", tag);
       }
 
+                  let recipeCarousel = document.querySelector(".carousel-inner");
+                  let numeroCarousel = document.querySelector(".numeroButton");
+                    recipeCarousel.innerHTML ="";
+                  let contenuCarousel = `    <div class="carousel-item active"> <div class="row g-3">`;
+                  let contenuNumero = `     <button type="button" data-bs-target="#cardCarousel" data-bs-slide-to="0" class="active bg-white text-black-50" href="#cardCarousel">1</button>`;
+
+                  let count = 0;
+                  let nbSlide = 1;
+                        recipesJSON.meals.forEach(meal=> {
+
+                  if (count > 11) {
+                    count = 0;
+                    nbSlide++;
+                    contenuCarousel += `</div></div><div class="carousel-item"><div class="row g-3">`;
+                  }
+
+                  contenuCarousel += `   <div class="col-6 col-lg-4">
+                    <div class="card shadow-sm h-100">
+                      <img src="${meal.strMealThumb}" class="card-img-top" alt="Card 1">
+                      <div class="card-body">
+                        <h5 class="card-title">${meal.strMeal}</h5>
+                        <p class="card-text">Description carte 1.</p>
+                      </div>
+                    </div>
+                  </div>`;
+                  count++;
+                  });
+                  for (let i = 1; i < nbSlide; i++) {
+                    contenuNumero += `     <button type="button" class=" bg-white text-black-50" data-bs-target="#cardCarousel" data-bs-slide-to="${i}"  href="#cardCarousel">${i+1}</button>`;
+                  }
+                  console.log(contenuCarousel);
+                   contenuCarousel += `</div></div>`;
+                  recipeCarousel.innerHTML = contenuCarousel;
+                  numeroCarousel.innerHTML = contenuNumero;
+      
             })
             .catch(err => console.error("Erreur :", err));
+        
+   
 
+      
             }
 
-            searchRecipe2("rice");
+            //search recipes from Spoonacular API
+           // searchRecipe2("rice");
 function searchRecipe2(tag){
     let APIKey="abccbe9f062c47618eceac3c27c0332f"
     const url = `https://api.spoonacular.com/recipes/complexSearch?query=${tag}&number=20&apiKey=${APIKey}`;
@@ -51,16 +110,60 @@ function searchRecipe2(tag){
 
             }
 
-  getRecipeDetails("635675");
+
+            //get recipe details from Spoonacular API
+  //getRecipeDetails("52772");
             async function getRecipeDetails(id) {
-                  let APIKey="abccbe9f062c47618eceac3c27c0332f"
-  let url = `https://api.spoonacular.com/recipes/635675/information?apiKey=${APIKey}`;
+  let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+   fetch(url)
+            .then(response => response.json())
+            .then(json => {
+              console.log(json);
+              data = json.meals[0];
+          console.log("Nom :", data.strMeal);
+
+  let fin = false;
+  console.log("Ingrédients :");
+  for(let i=1; i<20  && fin===false; i++){
+    let ingredient = data[`strIngredient${i}`];
+          if (ingredient === "") {
+                fin = true;
+              }
+          else
+          {
+          console.log(`Ingredient ${i} :` + ingredient+ ", measure : "+ data[`strMeasure${i}`]);
+          }
+  }
+
+  console.log("\nÉtapes :");
+  
+      console.log(` ${data.strInstructions}`);
+  
+    });
+    }
+
+    setStorage("635675");
+    function setStorage(data){
+      localStorage.setItem('recipeID', data);}
+
+      getStorage();
+      function getStorage(){
+      let data= localStorage.getItem("recipeID");
+      console.log(data); 
+      return   data;}
+
+      //get recipe details from Spoonacular API
+  //getRecipeDetails2("635675");
+            async function getRecipeDetails2(id) {
+  let url = ``;
    fetch(url)
             .then(response => response.json())
             .then(json => {
               console.log(json);
               data = json;
           console.log("Nom :", data.title);
+          console.log(`Image : ${data.image}`);
+
   console.log("Ingrédients :");
   data.extendedIngredients.forEach(ing => console.log("-", ing.original));
 
@@ -71,6 +174,7 @@ function searchRecipe2(tag){
     );
   } else {
     console.log("Aucune instruction disponible.");
+  
   }
 
     });
